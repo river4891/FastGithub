aebfb056a4e7445d1b8e896bce2949bca3d28d21
tower_town <FibonacciNuture@foxmail.com>
2023-08-23 12:28:11 +0800

    chore(service): add service configure for WSL and Alpine
    reference: WSL service #158 and Alpine Linux #229

--- pack.sh
+++ pack.sh
@@ -9,6 +9,11 @@ chmod 777 ./fastgithub_linux-x64/fastgithub
 chmod 777 ./fastgithub_linux-x64/dnscrypt-proxy/dnscrypt-proxy
 zip -r fastgithub_linux-x64.zip fastgithub_linux-x64
 
+# linux-x64
+chmod 777 ./fastgithub_linux-x64/fastgithub
+chmod 777 ./fastgithub_linux-x64/dnscrypt-proxy/dnscrypt-proxy
+zip -r fastgithub_linux-musl-x64.zip fastgithub_linux-musl-x64
+
 # linux-arm64
 chmod 777 ./fastgithub_linux-arm64/fastgithub
 chmod 777 ./fastgithub_linux-arm64/dnscrypt-proxy/dnscrypt-proxy
